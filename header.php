da730e9305d6c8a15c3e1d089c0dc2c02a0bd728
crazyyy <crazyyy@gmail.com>
2014-02-09 21:47:47 +0200

    tester
    Signed-off-by: crazyyy <crazyyy@gmail.com>

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
 		<!-- icons -->
-		<link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
+		<link href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico" rel="shortcut icon">
 		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 		<!-- css + javascript -->
