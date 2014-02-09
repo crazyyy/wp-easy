f96e6541a417a8d0aaa572aeadaf6ae1c585a483
crazyyy <crazyyy@gmail.com>
2014-02-09 20:49:41 +0200

    test git with sublime

--- header.php
+++ header.php
@@ -1,20 +1,20 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-		
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
+
 		<!-- dns prefetch -->
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-		
+
 		<!-- meta -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-		
+
 		<!-- icons -->
-		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+		<link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
 		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-			
+
 		<!-- css + javascript -->
 		<?php wp_head(); ?>
 		<script>
@@ -25,13 +25,13 @@
 		</script>
 	</head>
 	<body <?php body_class(); ?>>
-	
+
 		<!-- wrapper -->
 		<div class="wrapper">
-	
+
 			<!-- header -->
 			<header class="header clear" role="banner">
-				
+
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
@@ -40,12 +40,12 @@
 						</a>
 					</div>
 					<!-- /logo -->
-					
+
 					<!-- nav -->
 					<nav class="nav" role="navigation">
 						<?php html5blank_nav(); ?>
 					</nav>
 					<!-- /nav -->
-			
+
 			</header>
 			<!-- /header -->
